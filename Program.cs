2d7c4ea58ff53dd66c9c433772547568fbc160dd
Hugo <hugo.borrego02@gmail.com>
2024-11-12 11:02:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Smartphone nokia = new Nokia("123456789", "Nokia", "111111111111111", 64);
+        Console.WriteLine($"Número: {nokia.Numero}");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Smartphone iphone = new Iphone("987654321", "iPhone", "222222222222222", 128);
+        Console.WriteLine($"Número: {iphone.Numero}");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
